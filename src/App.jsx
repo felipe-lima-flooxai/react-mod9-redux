5313c6aa99e83c296416de191543c8825983d241
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-25 12:57:14 -0300

    feat: Intervalo como card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from './components/Card'
+import Intervalo from './components/Intervalo'
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <div className='App'>
         <h1>Exercicio React-Redux Simples</h1>
         <div className='linha'>
-          <Card></Card>
+          <Intervalo></Intervalo>
         </div>
         
       </div>
---
+++ src/components/Intervalo.jsx
@@ -0,0 +1,27 @@
+import "./Intervalo.css";
+import React from "react";
+import { connect } from "react-redux";
+
+import Card from "./Card";
+
+
+export default props =>{
+
+  return (
+    <Card title="Intervalo de Números" red>
+      <div className="Intervalo">
+        <span>
+          <strong>Mínino:</strong>
+          <input type="number" value={0} 
+             />
+        </span>
+        <span>
+          <strong>Máximo:</strong>
+          <input type="number" value={0}
+            />
+        </span>
+      </div>
+    </Card>
+  );
+}
+
