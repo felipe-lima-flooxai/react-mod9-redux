6c0a2c7aaf27f6e8f07eb5a9be30ebcbf09df271
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-25 11:59:00 -0300

    feat: arquivos iniciais

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './components/Card'
+
+
+function App() {
+
+  return (
+      <div className='App'>
+        <h1>Exercicio React-Redux Simples</h1>
+        <div className='linha'>
+          <Card></Card>
+        </div>
+        
+      </div>
+  )
+}
+
+export default App
